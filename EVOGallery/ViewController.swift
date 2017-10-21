a676bb48878f8928861eb353ab16e5679100f4bd
y.kostiukevych <y.kostiukevych@smartweb.com.ua>
2017-10-21 14:01:53 +0300

    Initial Commit

---
+++ EVOGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EVOGallery
+//
+//  Created by Костюкевич Илья on 21.10.2017.
+//  Copyright © 2017 Костюкевич Илья. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
